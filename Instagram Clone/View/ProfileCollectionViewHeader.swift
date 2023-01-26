11448073dea18b823f89d39aed0003262b82963b
Erick El nino <erick.tshimbombo@yahoo.com>
2023-01-26 02:56:01 +0200

    create the edit button

--- Instagram Clone/View/ProfileCollectionViewHeader.swift
+++ Instagram Clone/View/ProfileCollectionViewHeader.swift
@@ -23,13 +23,65 @@ class ProfileCollectionViewHeader: UICollectionReusableView
         return profileimage
     }()
     
+    private let nameLabel : UILabel =
+    {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Eriik Elnino"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let postLabel: UILabel =
+    {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        let attributed = NSAttributedString(string: "Post", attributes: [.font: UIFont.systemFont(ofSize: 14),.foregroundColor:UIColor.lightGray])
+        let MutabelAtributted = NSMutableAttributedString(string: "3 \n", attributes: [.font:  UIFont.boldSystemFont(ofSize: 16),.foregroundColor:UIColor.darkGray])
+        MutabelAtributted.append(attributed)
+        label.attributedText = MutabelAtributted
+        
+        return label
+    }()
+    
+    private let FollowingLabel: UILabel =
+    {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        let attributed = NSAttributedString(string: "Following", attributes: [.font: UIFont.boldSystemFont(ofSize: 14),.foregroundColor:UIColor.lightGray])
+        let MutabelAtributted = NSMutableAttributedString(string: "7 \n", attributes: [.font: UIFont.boldSystemFont(ofSize: 16),.foregroundColor:UIColor.darkGray])
+        MutabelAtributted.append(attributed)
+        label.attributedText = MutabelAtributted
+        return label
+    }()
+    
+    
+    private let FollowerLabel: UILabel =
+    {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        let attributed = NSAttributedString(string: "Followers", attributes: [.font: UIFont.systemFont(ofSize: 14),.foregroundColor:UIColor.lightGray])
+        let MutabelAtributted = NSMutableAttributedString(string: "5 \n", attributes: [.font: UIFont.boldSystemFont(ofSize: 16),.foregroundColor:UIColor.darkGray])
+        MutabelAtributted.append(attributed)
+        label.attributedText = MutabelAtributted
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
         layout()
         configured()
     }
-    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -39,16 +91,38 @@ extension ProfileCollectionViewHeader
 {
     private func style()
     {
-        self.backgroundColor = .lightGray
+      
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.profileImageView.backgroundColor = .systemRed
+        self.profileImageView.backgroundColor = .darkGray
         self.addSubview(profileImageView)
+        self.addSubview(nameLabel)
+        
+        
+        
+        
     }
     
     private func layout()
     {
+        let stack = UIStackView(arrangedSubviews: [postLabel,FollowingLabel,FollowerLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 10
+        
+        self.addSubview(stack)
+        
         NSLayoutConstraint.activate([profileImageView.topAnchor.constraint(equalToSystemSpacingBelow: self.topAnchor, multiplier: 3),
                                      profileImageView.leadingAnchor.constraint(equalToSystemSpacingAfter: self.leadingAnchor, multiplier: 2)
+        ])
+        
+        NSLayoutConstraint.activate([nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: profileImageView.bottomAnchor, multiplier: 1),
+                                     nameLabel.centerXAnchor.constraint(equalTo: profileImageView.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([stack.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+                                     stack.leadingAnchor.constraint(equalToSystemSpacingAfter: profileImageView.trailingAnchor, multiplier: 3),
+                                     self.trailingAnchor.constraint(equalToSystemSpacingAfter: stack.trailingAnchor, multiplier: 2)
         ])
     }
     
